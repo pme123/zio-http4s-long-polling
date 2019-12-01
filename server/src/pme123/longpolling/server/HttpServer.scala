a56b0a51e22c82ef874ec26d9476fd5b263dca5c
Pascal Mengelt <pascal.mengelt@gmail.com>
2019-12-01 18:30:15 +0100

    cleanup HttpServer

--- server/src/pme123/longpolling/server/HttpServer.scala
+++ server/src/pme123/longpolling/server/HttpServer.scala
@@ -7,7 +7,6 @@ import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.implicits._
-import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 import pme123.longpolling.server.NumberService.NumberServiceEnv
 import zio._
@@ -27,7 +26,7 @@ object HttpServer extends App {
 
   import dsl._
 
-  def service(): HttpRoutes[AppTask] =
+  def routes: HttpRoutes[AppTask] =
     HttpRoutes.of[AppTask] {
       case GET -> Root / IntVar(duration) =>
         NumberService.>.nextNumbers(Duration(duration, TimeUnit.SECONDS))
@@ -48,32 +47,37 @@ object HttpServer extends App {
       .flatMap { implicit rts =>
         for {
           _ <- zio.console.putStrLn("Server startup...")
-          qu <- Queue.bounded[Int](1000)
-          _ <- NumberProvider.>.run(rts).fork
-            .provideSome[ZEnv](_ =>
-              new Blocking.Live with NumberProvider.Live {
-                def queue: Queue[Int] = qu
-              })
-
-          httpApp = Router[AppTask](
-            "/" -> service()
-          ).orNotFound
-
-          program <- ZIO.runtime[AppEnvironment].flatMap { implicit rts =>
-            BlazeServerBuilder[AppTask]
-              .bindHttp(8088, "localhost")
-              .withHttpApp(httpApp)
-              .serve
-              .compile
-              .drain
-          }.provideSome[ZEnv](_ =>
-            new Console.Live with Clock.Live with Random.Live with NumberService.Live {
-              def queue: Queue[Int] = qu
-            })
+          queue <- Queue.bounded[Int](1000)
+          numberProvider <- numberProvider(rts, queue).fork
+          numberService <- numberService(queue).fork
           _ <- zio.console.putStrLn("Server startup finished")
-        } yield program
-
+          _ <- numberProvider.join
+          _ <- numberService.join
+          _ <- zio.console.putStrLn("Server shutdown finished")
+        } yield numberService
       }
+  }
+
+  private def numberProvider(rts: Runtime[zio.ZEnv], qu: Queue[Int]) = {
+    NumberProvider.>.run(rts)
+      .provideSome[ZEnv](_ =>
+        new Blocking.Live with NumberProvider.Live {
+          def queue: Queue[Int] = qu
+        })
+  }
+
+  private def numberService(qu: Queue[Int]) = {
+    ZIO.runtime[AppEnvironment].flatMap { implicit rts =>
+      BlazeServerBuilder[AppTask]
+        .bindHttp(8088, "localhost")
+        .withHttpApp(routes.orNotFound)
+        .serve
+        .compile
+        .drain
+    }.provideSome[ZEnv](_ =>
+      new Console.Live with Clock.Live with Random.Live with NumberService.Live {
+        def queue: Queue[Int] = qu
+      })
   }
 
 
